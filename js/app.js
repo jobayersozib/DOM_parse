931b58e83b838d2cc7fd52f0265f1d8c505316d1
jobayer sozib <zeni.sozibndc@gmail.com>
2016-08-26 23:41:48 +0000

    modify app.js and index.html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var i=0;
+var formdata=new FormData();
 function call(info){
     
     if(info.childNodes.length > 0){
@@ -7,12 +8,11 @@ function call(info){
             call(info.childNodes[i]);
         }
         else{
-            if(info.childNodes[i].nodeName==="INPUT"){
+            if(info.childNodes[i].tagName==="INPUT"){
             console.log(info.childNodes[i].value+"  "+info.childNodes[i].id+"  "+info.childNodes[i].type);
+               formdata.append(info.childNodes[i].id,info.childNodes[i].value) 
             }
-            else{
-                
-            }
+            
         }
     }
     }
@@ -24,13 +24,16 @@ function call(info){
 document.getElementById("post-data").addEventListener("click",function(e){
     
     var data=document.getElementById("form-data");
+    var data2=document.getElementsByTagName("textarea");
     for(var i=0;i<data.childNodes.length;i++){
-        if(data.childNodes[i].childNodes.length >0){
+        
             call(data.childNodes[i]);
-        }
-        else{
-            call(data.childNodes[i]);
-        }
+        
     }
+    
+       for (var key of formdata.keys()) {
+            console.log(formdata.getAll(key)); 
+}
+        
     
 });
